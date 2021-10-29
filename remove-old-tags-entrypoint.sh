903c900d06083b2d8e1c7fd00b93cfba79197106
Salva Carsi Moreno <scarsi@mercadona.es>
2021-10-29 10:40:44 +0200

    wip

--- remove-old-tags-entrypoint.sh
+++ remove-old-tags-entrypoint.sh
@@ -3,17 +3,17 @@ set -e
 
 echo '...............'
 
-git fetch --prune --prune-tags
-tags_to_remove=$(git tag --sort=-creatordate | tail -n +31)
-if [ -z "$tags_to_remove" ]
-then
-  exit 0
-fi
+# git fetch --prune --prune-tags
+# tags_to_remove=$(git tag --sort=-creatordate | tail -n +31)
+# if [ -z "$tags_to_remove" ]
+# then
+#   exit 0
+# fi
 
-echo "this tags are going to be removed: $tags_to_remove"
+# echo "this tags are going to be removed: $tags_to_remove"
 
-for tag in $tags_to_remove
-do
-  git tag --delete $tag
-  git push --no-verify --delete origin $tag
-done
+# for tag in $tags_to_remove
+# do
+#   git tag --delete $tag
+#   git push --no-verify --delete origin $tag
+# done
